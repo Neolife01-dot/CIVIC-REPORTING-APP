6501f532ff2344fc7b1b2d86f8242ada29665bb0
Great Ifeanyi Ukpai <greatifeanyi56@gmail.com>
2026-08-21 10:25:33 +0100

    Fix authentication form field IDs

--- js/auth.js
+++ js/auth.js
@@ -12,7 +12,7 @@ if (registerForm) {
 
         e.preventDefault();
 
-        const name = document.getElementById("name").value.trim();
+        const name = document.getElementById("fullname").value.trim();
         const email = document.getElementById("email").value.trim();
         const password = document.getElementById("password").value;
         const confirmPassword =
